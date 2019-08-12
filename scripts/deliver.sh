3e1b36be9bdbc219ac5f8afc2d7ea8d9e8b53429
Carlos Henrique da Silva Kafka <carlos_kafka@hotmail.com>
2019-08-11 23:12:33 -0300

    Update deliver.sh

--- scripts/deliver.sh
+++ scripts/deliver.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-docker build . -t carloskafka7/springboot-docker
+docker login
+
+echo "docker build . -t carloskafka7/springboot-docker"
+docker build . -t carloskafka7/springboot-docker 
+
+echo "docker push carloskafka7/springboot-docker"
 docker push carloskafka7/springboot-docker
